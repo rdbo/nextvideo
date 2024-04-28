8e602ad1d9f6dc1183579f504382e1f55088a587
rdbo <rdbodev@gmail.com>
2024-04-28 09:29:52 -0300

    added skeletons on main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,26 @@ import { Button } from "@/components/ui/button";
 import VideoSkeleton from "@/components/VideoSkeleton";
 
 export default function Home() {
+  const generateElements = (count: number, element) => {
+    let elements = [];
+    for (let i = 0; i < count; ++i) elements.push(element);
+    return elements;
+  };
+
   return (
-    <div className="px-4 py-2">
-      <h1 className="text-2xl font-bold mb-2">Latest Uploads</h1>
-      <VideoSkeleton />
-      <Button>Hello</Button>
-    </div>);
+    <div className="px-4">
+      <h1 className="text-2xl font-bold my-4">Latest Uploads</h1>
+      {generateElements(
+        3,
+        <div className="flex mb-8">
+          {generateElements(
+            5,
+            <div className="mr-4">
+              <VideoSkeleton />
+            </div>,
+          )}
+        </div>,
+      )}
+    </div>
+  );
 }
