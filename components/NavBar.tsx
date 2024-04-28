da5dbe744468748577a19fa0d3eebe36dc856414
rdbo <rdbodev@gmail.com>
2024-04-28 10:23:57 -0300

    added page navigation

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Search, Upload } from "lucide-react";
@@ -6,9 +7,11 @@ export default function NavBar() {
   return (
     <div className="px-2 py-1 relative h-14 flex items-center justify-between">
       <div>
-        <h1 className="font-bold text-2xl">
-          Next<span className="text-blue-500">Video</span>
-        </h1>
+        <Link href="/">
+          <h1 className="font-bold text-2xl">
+            Next<span className="text-blue-500">Video</span>
+          </h1>
+        </Link>
       </div>
       <div className="absolute top-0 left-0 bottom-0 right-0 w-min mx-auto flex justify-center items-center h-14">
         <Input
@@ -20,10 +23,12 @@ export default function NavBar() {
         </Button>
       </div>
       <div>
-        <Button variant="outline" className="rounded-full">
-          <Upload size={18} />
-          <p className="ml-1">Upload</p>
-        </Button>
+        <Link href="/upload">
+          <Button variant="outline" className="rounded-full">
+            <Upload size={18} />
+            <p className="ml-1">Upload</p>
+          </Button>
+        </Link>
       </div>
     </div>
   );
