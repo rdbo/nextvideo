8d44c9c547d5206dfb20ac4cab268ee9c0bb6ea8
rdbo <rdbodev@gmail.com>
2024-04-28 09:40:11 -0300

    use Array.from to generate ranges

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,22 @@ import { Button } from "@/components/ui/button";
 import VideoSkeleton from "@/components/VideoSkeleton";
 
 export default function Home() {
-  const generateElements = (count: number, element) => {
-    let elements = [];
-    for (let i = 0; i < count; ++i) elements.push(element);
-    return elements;
-  };
-
   return (
     <div className="px-4">
       <h1 className="text-2xl font-bold my-4">Latest Uploads</h1>
-      {generateElements(
-        3,
-        <div className="flex mb-8">
-          {generateElements(
-            5,
-            <div className="mr-4">
-              <VideoSkeleton />
-            </div>,
-          )}
-        </div>,
-      )}
+      {Array.from({ length: 3 }, (_, i) => {
+        return (
+          <div className="flex mb-8">
+            {Array.from({ length: 5 }, (_, i) => {
+              return (
+                <div className="mr-4">
+                  <VideoSkeleton key={i} />
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
