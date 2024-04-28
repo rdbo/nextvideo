80baac9d70db7315247e1fa0ff44646e64022a4b
rdbo <rdbodev@gmail.com>
2024-04-28 11:27:06 -0300

    added progress bar

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,24 +1,28 @@
 "use client";
 
+import { Progress } from "@/components/ui/progress";
 import { Upload } from "lucide-react";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 export default function UploadPage() {
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    if (acceptedFiles.length == 0) return;
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  const onDropAccepted = useCallback((acceptedFiles: File[]) => {
     console.log(acceptedFiles);
+    setIsUploading(true);
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop,
+    onDropAccepted,
     accept: { "video/*": [".mp4", ".avi", ".mpeg", ".mkv"] },
   });
 
   return (
     <>
       <h1 className="text-2xl font-bold text-center my-4">Upload a Video</h1>
-      <div className="flex justify-center px-8">
+      <div className="px-8">
         <div
           {...getRootProps()}
           className="border-4 border-slate-800 border-dashed h-96 w-full rounded-lg flex justify-center items-center text-2xl text-center text-slate-400"
@@ -26,11 +30,19 @@ export default function UploadPage() {
           <input {...getInputProps()} />
           <Upload size={32} />
           <p className="ml-2">
-            {isDragActive
-              ? "Drop the video file here..."
-              : "Select a video file"}
+            {isUploading
+              ? "Uploading..."
+              : isDragActive
+                ? "Drop the video file here..."
+                : "Select a video file"}
           </p>
         </div>
+        {isUploading && (
+          <div className="my-2 relative h-8 flex items-center justify-center">
+            <Progress value={uploadProgress} className="h-6"/>
+            <p className="absolute top-0 h-8 text-sm flex items-center font-bold text-blue-400">{uploadProgress}%</p>
+          </div>
+        )}
       </div>
     </>
   );
