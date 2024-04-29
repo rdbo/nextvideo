e9da695a000ea4e4645b6999977767d28315b37f
rdbo <rdbodev@gmail.com>
2024-04-29 09:24:46 -0300

    improved video skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import VideoSkeleton from "@/components/VideoSkeleton";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const loadVideo = (videoUrl: string) => {
+    router.push(videoUrl);
+  };
+
   return (
     <div className="pl-4">
-      <h1 className="text-2xl font-bold my-4 text-center">Latest Uploads</h1>
+      <h1 className="text-2xl font-bold my-4 text-center">Random Videos</h1>
       <div className="w-full flex flex-wrap justify-center">
         {Array.from({ length: 32 }, (_, i) => {
           return (
             <div key={i} className="mr-4 mb-8">
-              <VideoSkeleton />
+              <VideoSkeleton onClick={loadVideo} />
             </div>
           );
         })}
--- components/VideoSkeleton.tsx
+++ components/VideoSkeleton.tsx
@@ -1,15 +1,30 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { MouseEventHandler } from "react";
 
-export default function SkeletonCard() {
+export interface Props {
+  title?: string;
+  videoUrl?: string;
+  onClick: (videoUrl: string) => void;
+}
+
+export default function VideoSkeleton({ title, videoUrl, onClick }: Props) {
   return (
-    <div className="flex flex-col space-y-3 w-min">
+    <div
+      className={
+        "flex flex-col space-y-3 w-min" + (videoUrl ? "cursor-pointer" : "")
+      }
+      onClick={() => videoUrl && onClick(videoUrl)}
+    >
       <Skeleton className="h-40 w-full rounded-xl" />
-      <div className="flex items-center space-x-4">
-        <Skeleton className="h-12 w-12 rounded-full" />
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-60" />
-          <Skeleton className="h-4 w-48" />
-        </div>
+      <div className="space-y-2">
+        {title ? (
+          <h1>{title}</h1>
+        ) : (
+          <>
+            <Skeleton className="h-4 w-60" />
+            <Skeleton className="h-4 w-48" />
+          </>
+        )}
       </div>
     </div>
   );
